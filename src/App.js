2ae4887a8c5ebdf59c233c8e30d47013df94576a
Thomas Smulski <info@makenci.de>
2022-06-24 19:14:21 +0200

    form with bugs xD, update object by change input and not by add->button

--- src/App.js
+++ src/App.js
@@ -4,39 +4,42 @@ import ColorCard from "./components/ColorCard/ColorCard";
 import CardForm from "./components/Form/CardForm";
 import {nanoid} from "nanoid";
 
+const colorObjects = [
+  {
+    id: nanoid(),
+    colorCode: "#25B2D9",
+  },
+  {
+    id: nanoid(),
+    colorCode: "#A23A24",
+  },
+  {
+    id: nanoid(),
+    colorCode: "#879F18",
+  },
+];
 
 function App() {
-  const colorObjects = [
-    {
-      id: nanoid(),
-      colorCode: "#25B2D9",
-    },
-    {
-      id: nanoid(),
-      colorCode: "#A23A24",
-    },
-    {
-      id: nanoid(),
-      colorCode: "#879F18",
-    },
-  ];
-
   const [cards, setCards] = useState(colorObjects);
-  const [newCard, setNewCard] = useState();
+  const [colorChange, setColorChange] = useState();
 
   function editCard(value){
     setCards(value)
   }
 
+  function test(value){
+    setCards({value,...cards})
+  }
+
   function addNewCard(value){
-    setNewCard(value)
+    setColorChange(value)
   }
 
 
   return (
     <>
       <div className="card-container">
-        <CardForm newCard={newCard} addNewCard={addNewCard}/>
+        <CardForm colorChange={colorChange} addNewCard={addNewCard} cardObjects={cards} editCard={editCard}/>
         <ColorCard
           colorObjects={cards}
           editCard={editCard}
--- src/components/Form/CardForm.jsx
+++ src/components/Form/CardForm.jsx
@@ -4,18 +4,24 @@ import {nanoid} from "nanoid";
 
 
 
-export default function CardForm({ newCard, addNewCard }) {
+export default function CardForm({ colorChange, addNewCard, cardObjects, editCard}) {
   
     return (
        
             <form onSubmit={(e)=>{
                 e.preventDefault();
-                                                
+                const textInput = e.target.elements[0].value;
+                let newCard = {id:nanoid(), colorCode:textInput};
+                
+                let test2 = cardObjects;
+                test2.push(newCard);
+                editCard(test2);  
+                console.log(test2); 
+                                          
             }}>
-            <article className="card" style={{ backgroundColor: newCard }}>
+            <article className="card" style={{ backgroundColor: colorChange }}>
                 <input type="text" className="card__input" onChange={(e)=>{
                 addNewCard(e.target.value);
-                //addNewCard(newCard);
             }}>
             </input>
             <button className="card__button-add">ADD</button>
